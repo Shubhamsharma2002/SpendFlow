d4e19f36e26fddcf2e7f9f1b4bdc4378704fca66
Shubham Sharma <shubhamtech004@gmail.com>
2026-04-19 23:41:21 +0530

    project setup
    also done some layout setup , login,signup pages

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-background">
       <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
+        Welcome to Nativewind..!
       </Text>
       <Link
         href="/onboarding"
