53abeba5c937392c1b368fc2d01182e83ab091ec
Shubham Sharma <shubhamtech004@gmail.com>
2026-04-21 23:44:00 +0530

    project setup
    also done some layout setup , login,signup pages

--- app/index.tsx
+++ app/index.tsx
@@ -18,7 +18,7 @@ export default function App() {
         href="/(auth)/sign-in"
         className="mt-4 rounded bg-primary text-white p-4"
       >
-        Go To Sign in ...
+        Go To Sign in ... || 
       </Link>
       <Link
         href="/(auth)/sign-up"
