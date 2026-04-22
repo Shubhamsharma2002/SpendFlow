7596a8c76622f7eb35ef54ba738627ba54034b10
Shubham Sharma <shubhamtech004@gmail.com>
2026-04-22 23:38:44 +0530

    project setup
    also done some layout setup , login,signup pages

--- app/index.tsx
+++ app/index.tsx
@@ -24,7 +24,7 @@ export default function App() {
         href="/(auth)/sign-up"
         className="mt-4 rounded bg-primary text-white p-4"
       >
-        Go To SingUp ..
+        Go To SingUp..
       </Link>
     </View>
   );
